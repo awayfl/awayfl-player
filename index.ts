3f552479be63130fcd35a00bbad6a8d6a03ac08c
Rob Bateman <rob.bateman@gmail.com>
2020-02-26 00:01:52 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - AwayFL-Player - 0.1.5");
+console.debug("AwayFL - AwayFL-Player - 0.1.6");
 
 export {Player} from "./lib/Player";
 export {PlayerAVM1} from "./lib/PlayerAVM1";
