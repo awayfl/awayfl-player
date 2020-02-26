76ff2355d9f5cdb223ea62a7bf75d611b30b47c7
Rob Bateman <rob.bateman@gmail.com>
2020-02-26 00:01:37 +0000

    update scripts with colors and optimising logic

--- scripts/updateAwayDev_win.bat
+++ scripts/updateAwayDev_win.bat
@@ -1,72 +1,85 @@
+@ECHO off
 TITLE Update AwayJS Dev Enviroment
-ECHO Pulls all updates into the "@awayjs" and "@awayfl" directories, and runs tsc:build
+ECHO [32m Pulls all updates into the "@awayjs" and "@awayfl" directories, and runs tsc:build if required[0m
 PAUSE
 cd..
 cd..
 cd @awayjs
 
-ECHO Pull and build core
+ECHO [32m Pull and build @awayjs/core[0m
 cd core
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayjs/stage[0m
 cd stage
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayjs/renderer[0m
 cd renderer
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayjs/graphics[0m
 cd graphics
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayjs/materials[0m
 cd materials
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayjs/scene[0m
 cd scene
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayjs/view[0m
 cd view
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
 cd..
 cd @awayfl
 
+ECHO [32m Pull and build @awayfl/swf-loader[0m
 cd swf-loader
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayfl/avm1[0m
 cd avm1
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayfl/avm2[0m
 cd avm2
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
+ECHO [32m Pull and build @awayfl/playerglobal[0m
 cd playerglobal
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 cd..
 
 cd..
+
+ECHO [32m Pull and build awayfl-player[0m
 cd awayfl-player
-git pull
-call npm run tsc:build
+git pull | findstr /C:"Already up to date."
+IF %errorlevel%==1 call npm run tsc:build
 
 PAUSE
