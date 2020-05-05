2aefaccbbb63d708c2871c39d5402113b0e80790
80prozent <80prozent@differentdesign.de>
2020-05-05 23:21:27 +0200

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - AwayFL-Player - 0.2.7");
+console.debug("AwayFL - AwayFL-Player - 0.2.8");
 
 export {AVMPlayer} from "./lib/AVMPlayer";
 export {AVM1Player} from "./lib/AVM1Player";
