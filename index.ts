98b4ccb8d6594a35625567cf2e2962777ce9cb66
80prozent <80prozent@differentdesign.de>
2021-04-12 18:08:48 +0200

    export LoaderInfo and StageManager

--- index.ts
+++ index.ts
@@ -5,3 +5,5 @@ console.debug("AwayFL - AwayFL-Player - 0.2.23");
 export {AVMPlayer} from "./lib/AVMPlayer";
 export {AVM1Player} from "./lib/AVM1Player";
 export {AVM2Player} from "./lib/AVM2Player";
+export {LoaderEvent} from "@awayjs/core";
+export {StageManager} from "@awayjs/stage";
