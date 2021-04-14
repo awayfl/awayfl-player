1ac9a3ccae43d3e21db6b5b90f4f4914e0ae0a0b
80prozent <80prozent@differentdesign.de>
2021-04-14 12:56:35 +0200

    export core/EventBase

--- index.ts
+++ index.ts
@@ -6,4 +6,5 @@ export {AVMPlayer} from "./lib/AVMPlayer";
 export {AVM1Player} from "./lib/AVM1Player";
 export {AVM2Player} from "./lib/AVM2Player";
 export {LoaderEvent} from "@awayjs/core";
+export {EventBase} from "@awayjs/core";
 export {StageManager} from "@awayjs/stage";
