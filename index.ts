446be70880e26762c51668abdf6dffd2920361f9
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-06-07 14:53:53 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - AwayFL-Player - 0.2.28");
+console.debug("AwayFL - AwayFL-Player - 0.2.29");
 
 export {AVMPlayer} from "./lib/AVMPlayer";
 export {AVM1Player} from "./lib/AVM1Player";
