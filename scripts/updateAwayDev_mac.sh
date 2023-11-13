8162864e59bc3c746dade79f739b6212eb3fdbbe
Rob Bateman <rob@infiniteturtles.co.uk>
2023-11-13 12:59:00 +0000

    some tweaks for updateAwayDev batch for mac

--- scripts/updateAwayDev_mac.sh
+++ scripts/updateAwayDev_mac.sh
@@ -7,7 +7,6 @@ function updateIfNeeded () {
     UP_TO_DATE=`git pull origin $1 2>/dev/null | grep "Already up to date" | wc -l`
     set -e
     if [[ "$UP_TO_DATE" == 0 ]]; then
-        yarn
         npm run tsc:build
     fi
 }
@@ -19,69 +18,66 @@ cd ..
 
 cd @awayjs
 
-echo [32m Cloning "@awayjs" modules from Github[0m
-
-echo [32m Checkout and build "@awayjs/core" module if needed[0m
+echo [32m Pull and build "@awayjs/core"[0m
 cd core
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayjs/stage" module if needed[0m
+echo [32m Pull and build "@awayjs/stage"[0m
 cd stage
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayjs/view" module if needed[0m
+echo [32m Pull and build "@awayjs/view"[0m
 cd view
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayjs/renderer" module if needed[0m
+echo [32m Pull and build "@awayjs/renderer"[0m
 cd renderer
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayjs/graphics" module if needed[0m
+echo [32m Pull and build "@awayjs/graphics"[0m
 cd graphics
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayjs/materials" module if needed[0m
+echo [32m Pull and build "@awayjs/materials"[0m
 cd materials
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayjs/scene" module if needed[0m
+echo [32m Pull and build "@awayjs/scene"[0m
 cd scene
 updateIfNeeded dev
 cd ..
 
 cd ..
 
-echo [32m Cloning "@awayfl" modules from Github[0m
 cd @awayfl
 
-echo [32m Checkout and build "@awayfl/swf-loader" module if needed[0m
+echo [32m Pull and build "@awayfl/swf-loader"[0m
 cd swf-loader
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayfl/avm1" module if needed[0m
+echo [32m Pull and build "@awayfl/avm1"[0m
 cd avm1
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayfl/avm2" module if needed[0m
+echo [32m Pull and build "@awayfl/avm2"[0m
 cd avm2
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "@awayfl/playerglobal" module if needed[0m
+echo [32m Pull and build "@awayfl/playerglobal"[0m
 cd playerglobal
 updateIfNeeded dev
 cd ..
 
-echo [32m Checkout and build "awayfl-player" module if needed[0m
+echo [32m Pull and build "awayfl-player"[0m
 cd ..
 cd awayfl-player
 updateIfNeeded dev
