f57805b1a3fa627aee4df12c4f4c8d39a896dfc7
Rob Bateman <rob@infiniteturtles.co.uk>
2025-01-27 16:04:35 +0000

    add nape physics to lib

---
+++ lib/nape/index.ts
@@ -0,0 +1,80 @@
+import "./nape.max.js";
+
+//@ts-ignore
+const nape = self.nape;
+//@ts-ignore
+const zpp_nape = self.zpp_nape;
+
+const _PolygonConstructor = nape.shape.Polygon;
+const _Body = nape.phys.Body;
+const _Interactor = nape.phys.Interactor;
+
+
+//add toPoint
+//@ts-ignore
+nape.geom.Vec2.prototype.toPoint = function(output:any) {
+    output.x = this.x;
+    output.y = this.y;
+    return output;
+}
+
+// nape.shape.Polygon = class _NewPolygon extends nape.shape.Polygon {
+//     constructor(v: any, m: any, f: any) {
+//         //@ts-ignore
+//         v = v.value ? v.value: v;
+//         super(v, m, f);
+//     }
+// }
+
+// const _add = zpp_nape.util.ZNPList_ZPP_InteractionListener.prototype.add;
+// zpp_nape.util.ZNPList_ZPP_InteractionListener.prototype.add = function(o: any): any {
+//     let l = o.handleri;
+//     // ASFunction closure
+//     if( typeof l.value === 'function') {
+//         o.handleri = l.value.bind(l.receiver);
+//     }
+//     return _add.call(this, o) as any;
+// }
+// // restore bechaviour of version 
+// // https://github.com/deltaluca/nape/commit/18ffe75f1e943023f2608dc220660aeca2504898
+// // @ts-ignore
+// nape.phys.Body.prototype.worldToLocal = nape.phys.Body.prototype.worldPointToLocal;
+// // @ts-ignore
+// nape.phys.Body.prototype.localToWorld = nape.phys.Body.prototype.localPointToWorld;
+// //@ts-ignore
+// nape.phys.Body.prototype.applyWorldImpulse = nape.phys.Body.prototype.applyImpulse;
+
+// //@ts-ignore
+// nape.phys.Body.prototype.applyLocalForce = function(vector: nape.geom.Vec2, pos) {
+//     const iter = <nape.phys.Body> this;
+//     const dt = iter.space.zpp_inner.pre_dt;
+//     nape.phys.Body.prototype.applyImpulse.call(iter, vector.mul(dt, true), pos);
+// }
+
+// //@ts-ignore
+// nape.phys.Body.prototype.applyLocalImpulse = nape.phys.Body.prototype.applyImpulse;
+
+// // hack user data, in newes nape it a object, need inser fields to it
+// nape.phys.Body = class PathedBody extends _Body {
+//     constructor(type,position) {
+//         super(type, position);
+
+//         Object.defineProperty(this, 'userData', {
+//             //@ts-ignore
+//             get: _Interactor.prototype.get_userData,
+//             set: function(v) {
+//                 this.zpp_inner_i.userData = v;
+//             }
+//         })
+//     }
+// }
+
+// // restore
+// // https://github.com/deltaluca/nape/blob/0759bf48e2274d0f024bf491c2bf06d66d5060d6/cx-src/nape/geom/GeomPoly.cx#L216
+// // @ts-ignore
+// nape.geom.GeomPoly.prototype.simple_decomposition = nape.geom.GeomPoly.prototype.simpleDecomposition;
+// // @ts-ignore
+// nape.geom.GeomPoly.prototype.convex_decomposition = nape.geom.GeomPoly.prototype.convexDecomposition;
+
+
+export default { nape };
--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,61 @@
+import typescript from 'rollup-plugin-typescript2'
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import gzip from 'rollup-plugin-gzip';
 
-export default {
-	input: './dist/index.js',
-	output: {
-		name: 'awayflplayer',
-		sourcemap: true,
-		format: 'iife',
-		file: './bundle/awayfl-player.umd.js',
+export default [
+	{
+		input: './dist/index.js',
+		output: {
+			name: 'awayflplayer',
+			sourcemap: true,
+			format: 'iife',
+			file: './bundle/awayfl-player.umd.js',
+		},
+		plugins: [
+			nodeResolve(),
+			// {
+			//     transform(code, id) {
+			//         return code.replace(/\/\*\* @class \*\//g, "\/*@__PURE__*\/");
+			//     }
+			// },
+			commonjs(),
+			terser({
+				// mangle: {
+				// 	properties: {
+				// 		reserved: ['startPokiGame', 'userAgent', 'Number', '__constructor__', 'prototype']
+				// 	}
+				// }
+			}),
+			gzip()
+		]
 	},
-	plugins: [
-		nodeResolve(),
-		// {
-        //     transform(code, id) {
-        //         return code.replace(/\/\*\* @class \*\//g, "\/*@__PURE__*\/");
-        //     }
-        // },
-		commonjs(),
-		terser({
-			// mangle: {
-			// 	properties: {
-			// 		reserved: ['startPokiGame', 'userAgent', 'Number', '__constructor__', 'prototype']
-			// 	}
-			// }
-		}),
-		gzip()
-	]
-};
+	{
+		input: './lib/nape/index.ts',
+		output: {
+			name: 'AWAY_NAPE',
+			sourcemap: false,
+			format: 'iife',
+			file: './template/away_nape.js'
+		},
+		plugins: [
+			typescript(),
+			nodeResolve(),
+			// {
+			//     transform(code, id) {
+			//         return code.replace(/\/\*\* @class \*\//g, "\/*@__PURE__*\/");
+			//     }
+			// },
+			commonjs(),
+			terser({
+				// mangle: {
+				// 	properties: {
+				// 		reserved: ['startPokiGame', 'userAgent', 'Number', '__constructor__', 'prototype']
+				// 	}
+				// }
+			}),
+			gzip()
+		]
+	}
+]
