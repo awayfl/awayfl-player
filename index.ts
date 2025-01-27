7a97c0d6267529eb9d6eefc18a379e92c515e308
Rob Bateman <rob@infiniteturtles.co.uk>
2025-01-27 16:05:01 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { BitmapImage2D, ImageUtils } from "@awayjs/stage";
 
-console.debug("AwayFL-Player - 0.2.46");
+console.debug("AwayFL-Player - 0.2.47");
 
 export {AVMPlayer} from "./lib/AVMPlayer";
 export {AVM1Player} from "./lib/AVM1Player";
