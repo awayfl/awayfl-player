35c53e56c0350acabe395e3e6d2d801bf25e2412
Rob Bateman <rob@infiniteturtles.co.uk>
2025-09-29 23:36:13 +0100

    better fix for instanceof Array errors

--- lib/nape/index.ts
+++ lib/nape/index.ts
@@ -31,9 +31,7 @@ nape.geom.Vec2.fromPoint = function(point:any, param2:Boolean = false) {
 nape.geom.GeomPoly = class _NewGeomPoly extends nape.geom.GeomPoly {
     constructor(v: any) {
         //@ts-ignore
-        if (v?._buffer)
-            v = v._buffer;
-
+        v = v.value ? v.value: v;
         super(v);
     }
 }
@@ -42,15 +40,14 @@ nape.geom.GeomPoly = class _NewGeomPoly extends nape.geom.GeomPoly {
 nape.callbacks.InteractionCallback.axIsType = function(x: any): boolean {
     return x instanceof this;
 }
- 
 
-// nape.shape.Polygon = class _NewPolygon extends nape.shape.Polygon {
-//     constructor(v: any, m: any, f: any) {
-//         //@ts-ignore
-//         v = v.value ? v.value: v;
-//         super(v, m, f);
-//     }
-// }
+nape.shape.Polygon = class _NewPolygon extends nape.shape.Polygon {
+    constructor(v: any, m: any, f: any) {
+        //@ts-ignore
+        v = v.value ? v.value: v;
+        super(v, m, f);
+    }
+}
 
 // const _add = zpp_nape.util.ZNPList_ZPP_InteractionListener.prototype.add;
 // zpp_nape.util.ZNPList_ZPP_InteractionListener.prototype.add = function(o: any): any {
